a877464d8ec1354b67d08a36103e2e85e0951549
Jack Morrison <jack1morrison@sky.com>
2022-06-12 22:18:31 +0100

    Move font styling headers

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,12 +12,6 @@ const Header = ({ title }: Props) => (
       <title>{title == 'Jack Morrison' ? title : title + ' | Jack Morrison'}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet"
-      />
     </Head>
     <header>
       <nav>
