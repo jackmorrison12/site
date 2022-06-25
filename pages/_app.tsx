f7a3138d5773f97762859ddff9d29bf22f38abd0
Jack Morrison <jack1morrison@sky.com>
2022-06-25 23:28:48 +0100

    Add basic colour theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,16 @@ import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <ThemeProvider
+      attribute="class"
+      value={{
+        light: 'light-theme',
+        dark: 'dark-theme',
+        greenOrange: 'green-orange-theme',
+        bluePink: 'blue-pink-theme',
+      }}
+      themes={['light', 'dark', 'greenOrange', 'bluePink']}
+    >
       <Component {...pageProps} />
     </ThemeProvider>
   );
