c9306ca8ff04f74f838b25fea6796919a36971b8
Jack Morrison <jack1morrison@sky.com>
2022-07-13 23:52:13 +0100

    Add stub pages for footer links

---
+++ pages/fair-use.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout/Layout';
+
+const CreditsPage = () => (
+  <Layout title="Credits">
+    <h1>Credits</h1>
+    <p>Credits for the site</p>
+  </Layout>
+);
+
+export default CreditsPage;
