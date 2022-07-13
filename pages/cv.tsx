1e37b79c17ae4a25cb2197b218abe29580eb9c7f
Jack Morrison <jack1morrison@sky.com>
2022-07-13 22:44:54 +0100

    Add stub pages

---
+++ pages/cv.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Layout from '../components/Layout/Layout';
+
+const CVPage = () => (
+  <Layout title="Me">
+    <h1>CV</h1>
+    <p>This page will host my CV in react</p>
+    <p>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+    </p>
+  </Layout>
+);
+
+export default CVPage;
