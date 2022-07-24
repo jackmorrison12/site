126a998530e9be1d9aef24c3881767f00bb1a4c2
Jack Morrison <jack1morrison@sky.com>
2022-07-24 18:42:34 +0100

    Stop reporting web vitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,6 @@ import { GoogleAnalytics, usePageViews, event } from 'nextjs-google-analytics';
 
 import '../styles/index.css';
 
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  event(metric.name, {
-    category: metric.label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
-    value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value), // values must be integers
-    label: metric.id, // id unique to current page load
-    nonInteraction: true, // avoids affecting bounce rate.
-  });
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
   usePageViews();
   return (
