74336265b04444594e3cbd1a5b5002bfcb8396c2
Jack Morrison <32654372+jackmorrison12@users.noreply.github.com>
2022-07-24 19:15:41 +0100

    Add Google Analytics (#12)
    * Add google analytics
    
    * Stop reporting web vitals
    
    * Remove explicit usePageViews
    
    * Add event tracking on theme selection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { AppProps } from 'next/app';
+import { AppProps, NextWebVitalsMetric } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { GoogleAnalytics, usePageViews, event } from 'nextjs-google-analytics';
 
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      value={{
-        light: 'light-theme',
-        dark: 'dark-theme',
-        greenOrange: 'green-orange-theme',
-        bluePink: 'blue-pink-theme',
-      }}
-      themes={['light', 'dark', 'greenOrange', 'bluePink']}
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics />
+      <ThemeProvider
+        attribute="class"
+        value={{
+          light: 'light-theme',
+          dark: 'dark-theme',
+          greenOrange: 'green-orange-theme',
+          bluePink: 'blue-pink-theme',
+        }}
+        themes={['light', 'dark', 'greenOrange', 'bluePink']}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
