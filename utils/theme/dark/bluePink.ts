2ee0408438ec9d5b44d23506759d9ebaee8bdf9f
Jack Morrison <jack1morrison@sky.com>
2023-05-08 23:05:39 +0100

    Delete unused code

--- utils/theme/dark/bluePink.ts
+++ utils/theme/dark/bluePink.ts
@@ -33,17 +33,3 @@ export const BluePinkDarkTheme: Theme = {
     legacyGrey: { ...blue },
   },
 };
-
-// Grey:
-// 1: { hue: 0, saturation: 0, lightness: 8.5 },
-// 2: { hue: 0, saturation: 0, lightness: 11.0 },
-// 3: { hue: 0, saturation: 0, lightness: 13.6 },
-// 4: { hue: 0, saturation: 0, lightness: 15.8 },
-// 5: { hue: 0, saturation: 0, lightness: 17.9 },
-// 6: { hue: 0, saturation: 0, lightness: 20.5 },
-// 7: { hue: 0, saturation: 0, lightness: 24.3 },
-// 8: { hue: 0, saturation: 0, lightness: 31.2 },
-// 9: { hue: 0, saturation: 0, lightness: 43.9 },
-// 10: { hue: 0, saturation: 0, lightness: 49.4 },
-// 11: { hue: 0, saturation: 0, lightness: 62.8 },
-// 12: { hue: 0, saturation: 0, lightness: 93.0 },
