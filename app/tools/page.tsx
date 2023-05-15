f17661e11328f571c805fbc340255e098170770c
Jack Morrison <jack1morrison@sky.com>
2023-05-15 23:30:13 +0100

    Migrate MDX components

---
+++ app/tools/page.tsx
@@ -0,0 +1,9 @@
+import Tools from './tools.mdx';
+
+export default function Page() {
+  return <Tools />;
+}
+export const metadata = {
+  title: 'Tools Used',
+  description: 'What I used to make this site',
+};
