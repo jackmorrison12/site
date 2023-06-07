d2b182e5ef3e5afcd540e67324110a7083b49caf
Jack Morrison <jack1morrison@sky.com>
2023-06-07 11:53:40 -0400

    Update to use zod

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -3,14 +3,13 @@ import { getTopTracks } from '../../data-access/lastfm/api/getTopTracks';
 
 export default async function Page() {
   const data = await getTopTracks({ limit: '10' });
-  console.log(`Top tracks retrieved: ${JSON.stringify(data)}`);
   return (
     <>
       <Title value="LIVE" offset="-313.32" />
       <p>Live data on what I&apos;m up to!</p>
       <ul>
         {data.toptracks.track.map((t) => (
-          <li key={t.mbid}>
+          <li key={`${t.name} ${t.artist.name}`}>
             <a href={t.url}>
               {t.playcount} - {t.name}
             </a>
--- data-access/lastfm/api/getTopTracks.ts
+++ data-access/lastfm/api/getTopTracks.ts
@@ -1,4 +1,5 @@
 import { LASTFM_BASE_URL } from './urls';
+import { z } from 'zod';
 
 type TopTracksRequest = {
   user?: string;
@@ -7,37 +8,53 @@ type TopTracksRequest = {
   page?: string;
 };
 
-type TopTracksResponse = {
-  toptracks: {
-    track: Array<{
-      streamable: { fulltrack: string; '#text': string };
-      mbid: string;
-      name: string;
-      image: Array<{ size: 'small' | 'medium' | 'large' | 'extralarge'; '#text': string }>;
-      artist: { url: string; name: string; mbid: string };
-      url: string;
-      duration: string;
-      '@attr': { rank: string };
-      playcount: string;
-    }>;
-    '@attr': { user: 'JackMorrison12'; totalPages: string; page: string; perPage: string; total: string };
-  };
-};
+class LastFMError extends Error {}
+
+const topTracksSchema = z.object({
+  toptracks: z.object({
+    track: z.array(
+      z.object({
+        streamable: z.object({ fulltrack: z.string(), '#text': z.string() }),
+        mbid: z.string(),
+        name: z.string(),
+        image: z.array(z.object({ size: z.enum(['small', 'medium', 'large', 'extralarge']), '#text': z.string() })),
+        artist: z.object({ url: z.string(), name: z.string(), mbid: z.string() }),
+        url: z.string(),
+        duration: z.coerce.number(),
+        '@attr': z.object({ rank: z.string() }),
+        playcount: z.string(),
+      }),
+    ),
+    '@attr': z.object({
+      user: z.literal('Jackmorrison12'),
+      totalPages: z.string(),
+      page: z.string(),
+      perPage: z.string(),
+      total: z.string(),
+    }),
+  }),
+});
 
 export const getTopTracks = async ({ user = 'jackmorrison12', period, limit, page }: TopTracksRequest) => {
   // TODO update fetch params to be run once every few mins
-  const res = await fetch(
-    `${LASTFM_BASE_URL}?method=user.gettoptracks&user=${user}&api_key=${process.env.LASTFM_API_KEY}&format=json${
-      period ? `&period=${period}` : ''
-    }${limit ? `&limit=${limit}` : ''}${page ? `&page=${page}` : ''}`,
-  );
+  const url = `${LASTFM_BASE_URL}?method=user.gettoptracks&user=${user}&api_key=${
+    process.env.LASTFM_API_KEY
+  }&format=json${period ? `&period=${period}` : ''}${limit ? `&limit=${limit}` : ''}${page ? `&page=${page}` : ''}`;
+
+  const res = await fetch(url);
 
   if (!res.ok) {
     console.error(`Failed to fetch top tracks: ${JSON.stringify(res)}`);
     throw new Error('Failed to fetch top tracks');
   }
 
-  console.log(`Fetched top tracks: ${JSON.stringify(res)}`);
-  // TODO use Zod/Zodios to auto check this
-  return res.json() as Promise<TopTracksResponse>;
+  const parsedResult = topTracksSchema.safeParse(await res.json());
+
+  if (!parsedResult.success) {
+    console.error(`Failed to parse lastfm API response: ${parsedResult.error.issues}`);
+    throw new LastFMError();
+  }
+
+  console.log(`Fetched top tracks from LastFM API`);
+  return parsedResult.data;
 };
