2001a551b2fed4d132a18fb250c367eaea4480e2
Jack Morrison <jack1morrison@sky.com>
2023-06-08 21:33:18 -0400

    Add GitHub API

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,13 +1,26 @@
 import { Title } from '../../components/shared/Title';
+import { getEvents } from '../../data-access/github/api/getEvents';
 import { getRecentTracks } from '../../data-access/lastfm/api/getRecentTracks';
 import { getTopTracks } from '../../data-access/lastfm/api/getTopTracks';
 
 export default async function Page() {
   const topTracks = await getTopTracks({ limit: 10 });
   const recentTracks = await getRecentTracks({});
+
+  const events = await getEvents({ perPage: 10 });
+  console.log(events);
+
   return (
     <>
       <Title value="LIVE" offset="-313.32" />
+      <p>GitHub Events</p>
+      <ul>
+        {events.map((e) => (
+          <li key={e.id}>
+            {e.type} in {e.repo.name}
+          </li>
+        ))}
+      </ul>
       <p>Recent tracks</p>
       <ul>
         {recentTracks.recenttracks.track.map((t) => (
