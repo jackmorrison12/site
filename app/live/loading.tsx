508990e71b499fa8930563f079a37b4ae1ca9c6f
Jack Morrison <jack1morrison@sky.com>
2023-06-08 22:02:14 -0400

    Add loading pages

---
+++ app/live/loading.tsx
@@ -0,0 +1,14 @@
+import { Title } from '../../components/shared/Title';
+export default async function Page() {
+  return (
+    <>
+      <Title value="LIVE" offset="-313.32" />
+      <p>GitHub Events</p>
+      <p>Loading...</p>
+      <p>Recent tracks</p>
+      <p>Loading...</p>
+      <p>Top tracks</p>
+      <p>Loading...</p>
+    </>
+  );
+}
