ea52bba9b61ee127aae743db0564ee94cad08f6d
Jack Morrison <jack1morrison@sky.com>
2023-07-15 23:59:43 +0100

    Add basic github page

---
+++ app/me/github/page.tsx
@@ -0,0 +1,83 @@
+import { formatInTimeZone } from 'date-fns-tz';
+
+import { useLivePage } from '../../live/live.hooks';
+
+export default async function Page() {
+  const { events } = await useLivePage();
+
+  return (
+    <>
+      <h1>
+        Recent GitHub Activity{' '}
+        <span style={{ fontSize: 'var(--fontSizes_small)' }}>
+          (in {formatInTimeZone(new Date(), 'Europe/London', 'z') === 'GMT' ? 'GMT' : 'BST'})
+        </span>
+      </h1>
+      <div style={{ display: 'flex', flexDirection: 'row', paddingTop: '20px' }}>
+        <div
+          style={{
+            height: '100%',
+            width: '5px',
+            backgroundColor: 'var(--colours_primary_default)',
+            marginRight: '20px',
+            marginLeft: '20px',
+          }}
+        ></div>
+        <div>
+          {Object.entries(events).map(([date, evts]) => (
+            <div key={date} style={{ paddingBottom: '30px' }}>
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  right: '42.5px',
+                  position: 'relative',
+                  alignItems: 'center',
+                  gap: '10px',
+                }}
+              >
+                <div
+                  style={{
+                    padding: '15px',
+                    borderRadius: '100px',
+                    backgroundColor: 'var(--colours_background_default)',
+                    border: '5px solid var(--colours_primary_default)',
+                  }}
+                />
+                <p
+                  style={{
+                    fontWeight: 'var(--fontWeights_thick)',
+                    textTransform: 'uppercase',
+                  }}
+                >
+                  {date}
+                </p>
+              </div>
+              {evts.map(
+                (e) =>
+                  e.message && (
+                    <div
+                      key={e.id}
+                      style={{ display: 'flex', flexDirection: 'row', gap: '10px', paddingBottom: '5px' }}
+                    >
+                      <p
+                        style={{
+                          fontWeight: 'var(--fontWeights_thick)',
+                          paddingLeft: '30px',
+                          flex: 'none',
+                          minWidth: '80px',
+                        }}
+                      >
+                        {e.created_at ? `${formatInTimeZone(new Date(e.created_at), 'Europe/London', 'h:mm aa')} ` : ''}
+                      </p>
+                      <p>{e.message}</p>
+                    </div>
+                  ),
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
