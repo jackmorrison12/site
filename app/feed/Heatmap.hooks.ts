e09ea06a88eee1fb9b33e691d3877072316491bb
Jack Morrison <jack1morrison@sky.com>
2023-07-22 21:41:32 +0100

    Add github timeline to feed

---
+++ app/feed/Heatmap.hooks.ts
@@ -0,0 +1,59 @@
+import _ from 'lodash';
+import { formatInTimeZone, utcToZonedTime } from 'date-fns-tz';
+
+import { getEvents } from '../../data-access/github/api/getEvents';
+
+export const useHeatmap = async () => {
+  const NUM_WEEKS = 9;
+  const ONE_WEEK = 60 * 60 * 1000 * 24 * 7;
+
+  // Zero indexed day of week
+  const dayOfWeek = Number(formatInTimeZone(new Date(), 'Europe/London', 'i')) - 1;
+  const currentDate = new Date(utcToZonedTime(new Date(), 'Europe/London').toDateString());
+  currentDate.setDate(currentDate.getDate() - dayOfWeek);
+
+  // Get the current monday 7 format it
+
+  // dd/mm for the last NUM_WEEKS mondays
+  const xLabels = new Array(NUM_WEEKS)
+    .fill(0)
+    .map((_, i) => formatInTimeZone(new Date(currentDate.getTime() - ONE_WEEK * i), 'Europe/London', 'dd/LL'))
+    .reverse();
+
+  const yLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  const evts = await getEvents({ perPage: 100 });
+
+  const currentDay = new Date(utcToZonedTime(new Date(), 'Europe/London').toDateString()).valueOf();
+
+  const groupedEvents = _.groupBy(evts, (e) => {
+    if (!e.created_at) {
+      return '';
+    }
+    const eventDay = new Date(utcToZonedTime(e.created_at, 'Europe/London').toDateString()).valueOf();
+
+    const diffTime = Math.abs(currentDay.valueOf() - eventDay.valueOf());
+    return Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  });
+
+  const daysShown = (NUM_WEEKS - 1) * 7 + dayOfWeek + 1;
+
+  const data1d = new Array(daysShown).fill(0);
+
+  Object.entries(groupedEvents).forEach(([k, v]) => {
+    if (Number(k) < daysShown) {
+      data1d[daysShown - Number(k) - 1] = v.length;
+    }
+  });
+
+  const data2d = _.compact(
+    data1d.map(function (el, i) {
+      if (i % 7 === 0) {
+        return data1d.slice(i, i + 7);
+      }
+    }),
+  );
+  const data = data2d[0].map((_, colIndex) => data2d.map((row) => row[colIndex] ?? 0));
+
+  return { data, xLabels, yLabels };
+};
--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import { Title } from '../../components/shared/Title';
+import { GitHubIcon } from '../me/logos/github';
+import { Heatmap } from './Heatmap';
+import { useHeatmap } from './Heatmap.hooks';
 import styles from './feed.module.scss';
 
 export default async function Page() {
+  const { data, xLabels, yLabels } = await useHeatmap();
+
   return (
     <>
       <Title value="FEED" offset="-310.76" />
@@ -60,7 +66,34 @@ export default async function Page() {
           </ul>
         </div>
         <div className={styles.sidebar}>
-          <div className={styles.github}>Github Activity</div>
+          <Link passHref href="/feed/github">
+            <div
+              className={styles.github}
+              style={{
+                backgroundColor: 'var(--colours_secondary_default)',
+                width: 'fit-content',
+                padding: '20px',
+                borderRadius: '10px',
+                color: 'var(--colours_text_default)',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: '10px',
+                  paddingBottom: '10px',
+                }}
+              >
+                <div style={{ width: '30px' }}>
+                  <GitHubIcon />
+                </div>
+                <h3>Recent Activity</h3>
+              </div>
+              <Heatmap data={data} xLabels={xLabels} yLabels={yLabels} />
+            </div>
+          </Link>
           <div className={styles.lastfm}>LastFM Activity</div>
         </div>
       </div>
