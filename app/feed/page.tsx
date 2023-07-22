e6e83b6850f1752f6b9dadcf7d7cb8cfa57d984a
Jack Morrison <jack1morrison@sky.com>
2023-07-22 01:25:27 +0100

    Add feed title

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,66 +1,70 @@
+import { Title } from '../../components/shared/Title';
 import styles from './feed.module.scss';
 
 export default async function Page() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.main}>
-        <h1>Recent Activity</h1>
-        <ul>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-        </ul>
+    <>
+      <Title value="FEED" offset="-310.76" />
+      <div className={styles.wrapper}>
+        <div className={styles.main}>
+          <h1>Recent Activity</h1>
+          <ul>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+          </ul>
+        </div>
+        <div className={styles.sidebar}>
+          <div className={styles.github}>Github Activity</div>
+          <div className={styles.lastfm}>LastFM Activity</div>
+        </div>
       </div>
-      <div className={styles.sidebar}>
-        <div className={styles.github}>Github Activity</div>
-        <div className={styles.lastfm}>LastFM Activity</div>
-      </div>
-    </div>
+    </>
   );
 }
 
