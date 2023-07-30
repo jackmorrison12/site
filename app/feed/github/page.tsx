c5a3e9f95fd940dca1e7378ba4f0661b1617e4d6
Jack Morrison <jack1morrison@sky.com>
2023-07-30 17:56:23 +0100

    Update github page

--- app/feed/github/page.tsx
+++ app/feed/github/page.tsx
@@ -13,61 +13,20 @@ export default async function Page() {
         Recent GitHub Activity
         <span className={styles.timezone}>in {timezone}</span>
       </h1>
-      <div style={{ display: 'flex', flexDirection: 'row', paddingTop: '20px' }}>
-        <div
-          style={{
-            height: '100%',
-            width: '5px',
-            backgroundColor: 'var(--colours_primary_default)',
-            marginRight: '20px',
-            marginLeft: '20px',
-          }}
-        ></div>
+      <div className={styles.timelineWrapper}>
+        <div className={styles.verticalLine} />
         <div>
           {Object.entries(events).map(([date, evts]) => (
-            <div key={date} style={{ paddingBottom: '30px' }}>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  right: '42.5px',
-                  position: 'relative',
-                  alignItems: 'center',
-                  gap: '10px',
-                }}
-              >
-                <div
-                  style={{
-                    padding: '15px',
-                    borderRadius: '100px',
-                    backgroundColor: 'var(--colours_background_default)',
-                    border: '5px solid var(--colours_primary_default)',
-                  }}
-                />
-                <p
-                  style={{
-                    fontWeight: 'var(--fontWeights_thick)',
-                    textTransform: 'uppercase',
-                  }}
-                >
-                  {date}
-                </p>
+            <div key={date} className={styles.contentGroup}>
+              <div className={styles.dateGroup}>
+                <div className={styles.circle} />
+                <p className={styles.date}>{date}</p>
               </div>
               {evts.map(
                 (e) =>
                   e.message && (
-                    <div
-                      key={e.id}
-                      style={{ display: 'flex', flexDirection: 'row', gap: '10px', paddingBottom: '5px' }}
-                    >
-                      <p
-                        style={{
-                          fontWeight: 'var(--fontWeights_thick)',
-                          paddingLeft: '30px',
-                          flex: 'none',
-                          minWidth: '80px',
-                        }}
-                      >
+                    <div key={e.id} className={styles.eventsGroup}>
+                      <p className={styles.time}>
                         {e.created_at ? `${formatInTimeZone(new Date(e.created_at), 'Europe/London', 'h:mm aa')} ` : ''}
                       </p>
                       <p>{e.message}</p>
