44a448206107019c205988aa38631b72894c425c
Jack Morrison <jack1morrison@sky.com>
2023-11-23 00:32:16 +0000

    Make footer links cooler

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -17,26 +17,38 @@ const Footer = () => (
       </div>
       <div className={styles.useful}>
         <h4>Fun(ish) Things</h4>
-        <Link href="/credits" passHref>
-          <p>Credits</p>
-        </Link>
-        <Link href="/tools" passHref>
-          <p>Tools Used</p>
-        </Link>
-        <Link href="/fair-use" passHref>
-          <p>Fair Use Policy</p>
-        </Link>
-        <Link href="/archive" passHref>
-          <p>Site Archive</p>
-        </Link>
+        <div className={styles.linksWrapper}>
+          <Link href="/credits" passHref className={styles.link}>
+            <p>Credits</p>
+          </Link>
+          <Link href="/tools" passHref className={styles.link}>
+            <p>Tools Used</p>
+          </Link>
+          <Link href="/fair-use" passHref className={styles.link}>
+            <p>Fair Use Policy</p>
+          </Link>
+          <Link href="/archive" passHref className={styles.link}>
+            <p>Site Archive</p>
+          </Link>
+        </div>
       </div>
       <div className={styles.popular}>
         <h4>Popular</h4>
         {/* Generate these from analytics on build*/}
-        <p>Live</p>
-        <p>Long Article Title 1</p>
-        <p>Something else people seem to like</p>
-        <p>Cool project I worked on</p>
+        <div className={styles.linksWrapper}>
+          <Link href="/" passHref className={styles.link}>
+            <p>Live</p>
+          </Link>
+          <Link href="/" passHref className={styles.link}>
+            <p>Long Article Title 1</p>
+          </Link>
+          <Link href="/" passHref className={styles.link}>
+            <p>Something else people seem to like</p>
+          </Link>
+          <Link href="/" passHref className={styles.link}>
+            <p>Cool project I worked on</p>
+          </Link>
+        </div>
       </div>
       <div className={styles.socials}>
         <h4>Socials</h4>
