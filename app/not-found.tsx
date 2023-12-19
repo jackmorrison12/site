8899a5377f6035aa70c4af19b2b44ec1143f2912
Jack Morrison <jack1morrison@sky.com>
2023-12-19 00:55:16 +0000

    Fetch in client component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,37 +1,43 @@
-// import Link from 'next/link';
-// import { headers } from 'next/headers';
+'use client';
 import { Title } from 'components/shared/Title';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useQuery } from 'react-query';
 
 export default function NotFound() {
-  //   const heads = headers();
-  //   const pathname = heads.get('x-invoke-path') || '';
-
-  //   const v0 = await fetch(`https://v0.jackmorrison.xyz${pathname}`);
-  //   const v1 = await fetch(`https://v1.jackmorrison.xyz${pathname}`);
-  //   const v2 = await fetch(`https://v2.jackmorrison.xyz${pathname}`);
-  //   const v4 = await fetch(`https://v4.jackmorrison.xyz${pathname}`);
+  const pathname = usePathname().slice(0);
+  const { isLoading, data } = useQuery(
+    ['notFound', { pathname }],
+    async () => await (await fetch(`/api/checkArchives?path=${pathname}`)).json(),
+  );
 
   return (
     <>
       <Title value="404" offset="-865.96" bgOverride="NOTFOUND" />
-      {/* <h1>{pathname} can&apos;t be found</h1>
-      {v0.status === 200 && (
-        <a href={`https://v0.jackmorrison.xyz${pathname}`}>This page is available on v0 of my website</a>
+      {isLoading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          <h1>{pathname} can&apos;t be found</h1>
+          {data.v0 === 200 && (
+            <a href={`https://v0.jackmorrison.xyz${pathname}`}>This page is available on v0 of my website</a>
+          )}
+          {data.v1 === 200 && (
+            <a href={`https://v1.jackmorrison.xyz${pathname}`}>This page is available on v1 of my website</a>
+          )}
+          {data.v2 === 200 && (
+            <a href={`https://v2.jackmorrison.xyz${pathname}`}>This page is available on v2 of my website</a>
+          )}
+          {data.v4 === 200 && (
+            <a href={`https://v4.jackmorrison.xyz${pathname}`}>This page is available on v4 of my website</a>
+          )}
+          {data.v0 !== 200 && data.v1 !== 200 && data.v2 !== 200 && data.v4 !== 200 && (
+            <p>
+              Return <Link href="/">home</Link>
+            </p>
+          )}
+        </>
       )}
-      {v1.status === 200 && (
-        <a href={`https://v1.jackmorrison.xyz${pathname}`}>This page is available on v1 of my website</a>
-      )}
-      {v2.status === 200 && (
-        <a href={`https://v2.jackmorrison.xyz${pathname}`}>This page is available on v2 of my website</a>
-      )}
-      {v4.status === 200 && (
-        <a href={`https://v4.jackmorrison.xyz${pathname}`}>This page is available on v4 of my website</a>
-      )}
-      {v0.status !== 200 && v1.status !== 200 && v2.status !== 200 && v4.status !== 200 && (
-        <p>
-          Return <Link href="/">home</Link>
-        </p>
-      )} */}
     </>
   );
 }
