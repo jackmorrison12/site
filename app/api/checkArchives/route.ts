9249256a705113197c721de15d97887edfa81173
Jack Morrison <jack1morrison@sky.com>
2023-12-19 00:10:40 +0000

    Update 404 page to show previous site iterations

---
+++ app/api/checkArchives/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const path = searchParams.get('path');
+  if (!path) {
+    return new Response('No path parameter given', { status: 500 });
+  }
+
+  const v0 = await fetch(`https://v0.jackmorrison.xyz/${path}`);
+  const v1 = await fetch(`https://v1.jackmorrison.xyz/${path}`);
+  const v2 = await fetch(`https://v2.jackmorrison.xyz/${path}`);
+  const v4 = await fetch(`https://v4.jackmorrison.xyz/${path}`);
+
+  console.log('IN PATH');
+  console.log(v4.status);
+
+  return NextResponse.json({ v0: v0.status, v1: v1.status, v2: v2.status, v4: v4.status });
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { headers } from 'next/headers';
+import { Title } from 'components/shared/Title';
+
+export default async function NotFound() {
+  const heads = headers();
+  const pathname = heads.get('x-invoke-path') || '';
+
+  const v0 = await fetch(`https://v0.jackmorrison.xyz${pathname}`);
+  const v1 = await fetch(`https://v1.jackmorrison.xyz${pathname}`);
+  const v2 = await fetch(`https://v2.jackmorrison.xyz${pathname}`);
+  const v4 = await fetch(`https://v4.jackmorrison.xyz${pathname}`);
+
+  return (
+    <>
+      <Title value="404" offset="-865.96" bgOverride="NOTFOUND" />
+      <h1>{pathname} can&apos;t be found</h1>
+      {v0.status === 200 && (
+        <a href={`https://v0.jackmorrison.xyz${pathname}`}>This page is available on v0 of my website</a>
+      )}
+      {v1.status === 200 && (
+        <a href={`https://v1.jackmorrison.xyz${pathname}`}>This page is available on v1 of my website</a>
+      )}
+      {v2.status === 200 && (
+        <a href={`https://v2.jackmorrison.xyz${pathname}`}>This page is available on v2 of my website</a>
+      )}
+      {v4.status === 200 && (
+        <a href={`https://v4.jackmorrison.xyz${pathname}`}>This page is available on v4 of my website</a>
+      )}
+      {v0.status !== 200 && v1.status !== 200 && v2.status !== 200 && v4.status !== 200 && (
+        <p>
+          Return <Link href="/">home</Link>
+        </p>
+      )}
+    </>
+  );
+}
