dd8736c00ea3b56609410cf79192cfa620b1ccd8
Jack Morrison <jack1morrison@sky.com>
2024-08-18 15:18:09 -0400

    Fix prettier

--- public/fallback-development.js
+++ public/fallback-development.js
@@ -6,44 +6,54 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./node_modules/@ducanh2912/next-pwa/dist/fallback.js":
-/*!************************************************************!*\
+/******/ (() => {
+  // webpackBootstrap
+  /******/ 'use strict';
+  /******/ var __webpack_modules__ = {
+    /***/ './node_modules/@ducanh2912/next-pwa/dist/fallback.js':
+      /*!************************************************************!*\
   !*** ./node_modules/@ducanh2912/next-pwa/dist/fallback.js ***!
   \************************************************************/
-/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+      /***/ (__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+        eval(
+          '__webpack_require__.r(__webpack_exports__);\n\nself.fallback = async (_)=>{\n    let { destination: e, url: A } = _, s = {\n        document: "/~offline",\n        image: false,\n        audio: false,\n        video: false,\n        font: false\n    }, c = s[e];\n    return c ? caches.match(c, {\n        ignoreSearch: !0\n    }) : "" === e && false && 0 ? 0 : Response.error();\n};\n\n\n//# sourceURL=webpack:///./node_modules/@ducanh2912/next-pwa/dist/fallback.js?',
+        );
 
-eval("__webpack_require__.r(__webpack_exports__);\n\nself.fallback = async (_)=>{\n    let { destination: e, url: A } = _, s = {\n        document: \"/~offline\",\n        image: false,\n        audio: false,\n        video: false,\n        font: false\n    }, c = s[e];\n    return c ? caches.match(c, {\n        ignoreSearch: !0\n    }) : \"\" === e && false && 0 ? 0 : Response.error();\n};\n\n\n//# sourceURL=webpack:///./node_modules/@ducanh2912/next-pwa/dist/fallback.js?");
+        /***/
+      },
 
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./node_modules/@ducanh2912/next-pwa/dist/fallback.js"](0, __webpack_exports__, __webpack_require__);
-/******/ 	
-/******/ })()
-;
+    /******/
+  };
+  /************************************************************************/
+  /******/ // The require scope
+  /******/ var __webpack_require__ = {};
+  /******/
+  /************************************************************************/
+  /******/ /* webpack/runtime/make namespace object */
+  /******/ (() => {
+    /******/ // define __esModule on exports
+    /******/ __webpack_require__.r = (exports) => {
+      /******/ if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+        /******/ Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+        /******/
+      }
+      /******/ Object.defineProperty(exports, '__esModule', { value: true });
+      /******/
+    };
+    /******/
+  })();
+  /******/
+  /************************************************************************/
+  /******/
+  /******/ // startup
+  /******/ // Load entry module and return exports
+  /******/ // This entry module can't be inlined because the eval devtool is used.
+  /******/ var __webpack_exports__ = {};
+  /******/ __webpack_modules__['./node_modules/@ducanh2912/next-pwa/dist/fallback.js'](
+    0,
+    __webpack_exports__,
+    __webpack_require__,
+  );
+  /******/
+  /******/
+})();
