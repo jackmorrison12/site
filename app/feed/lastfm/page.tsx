a9a7336f3ea89860e62451506ac6f535c2d7bda2
Jack Morrison <jack1morrison@sky.com>
2024-08-20 23:57:41 -0400

    Add artist page

--- app/feed/lastfm/page.tsx
+++ app/feed/lastfm/page.tsx
@@ -1,9 +1,10 @@
 import styles from './lastfm.module.scss';
 import { db } from 'drizzle/db';
 import { listens, tracks } from 'drizzle/schema';
-import { count, desc, eq, gte } from 'drizzle-orm';
+import { count, desc, eq, gte, sum } from 'drizzle-orm';
 import Image from 'next/image';
 import { RefreshButton } from './RefreshButton';
+import Link from 'next/link';
 
 export default async function Page() {
   const topTracks = await db
@@ -16,21 +17,53 @@ export default async function Page() {
     })
     .from(listens)
     .innerJoin(tracks, eq(listens.id, tracks.id))
-    .where(gte(listens.time, new Date(2024, 1, 1)))
+    .where(gte(listens.time, new Date(2024, 0, 1)))
     .groupBy(listens.id, tracks.name, tracks.artist, tracks.id, tracks.imageUrl)
     .orderBy(desc(count(listens.id)))
+    .limit(100);
+
+  const sq = db
+    .select({
+      id: listens.id,
+      count: count(listens.id).as('count'),
+    })
+    .from(listens)
+    .where(gte(listens.time, new Date(2024, 0, 1)))
+    .groupBy(listens.id)
+    .orderBy(desc(count(listens.id)))
+    .as('sq');
+
+  const topArtists = await db
+    .select({ artist: tracks.artist, sum: sum(sq.count) })
+    .from(tracks)
+    .innerJoin(sq, eq(tracks.id, sq.id))
+    .groupBy(tracks.artist)
+    .orderBy(desc(sum(sq.count)))
     .limit(100);
 
   return (
     <div>
       <h1>My 2024 Spotify Wrapped... so far</h1>
       <RefreshButton />
-      {topTracks.map((t) => (
-        <div key={t.id} className={styles.trackWrapper}>
-          {t.imageUrl && <Image src={t.imageUrl} width={50} height={50} alt={`Cover art for ${t.name}`} />}
-          {t.name} - {t.artist} - {t.count} listens
+      <div className={styles.listsWrapper}>
+        <div>
+          <h2>Top Tracks</h2>
+          {topTracks.map((t) => (
+            <div key={t.id} className={styles.trackWrapper}>
+              {t.imageUrl && <Image src={t.imageUrl} width={50} height={50} alt={`Cover art for ${t.name}`} />}
+              {t.name} - <Link href={`/feed/lastfm/artist/${t.artist}`}>{t.artist}</Link> - {t.count} listens
+            </div>
+          ))}
         </div>
-      ))}
+        <div>
+          <h2>Top Artists</h2>
+          {topArtists.map((a) => (
+            <div key={a.artist}>
+              <Link href={`/feed/lastfm/artist/${a.artist}`}>{a.artist}</Link> - {a.sum} listens
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
