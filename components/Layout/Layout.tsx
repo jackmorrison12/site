8183108f6c58cd6fc52ebfb70876775191b299c2
Milo Assistant <jacksassistantmilo@gmail.com>
2026-02-18 21:14:41 -0500

    feat: Integrate FooterWithHover into Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Layout.module.scss';
 
 import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import FooterWithHover from './Footer/FooterWithHover';
 import { LayoutProps } from './Layout.types';
 
 // should there be a separate textarea layout, or a prop? just for smaller margins
@@ -12,6 +12,6 @@ export const Layout = ({ children, title = 'Jack Morrison' }: LayoutProps) => (
   <div className={styles.app}>
     <Header title={title} />
     <main className={styles.content}>{children}</main>
-    <Footer />
+    <FooterWithHover />
   </div>
 );
